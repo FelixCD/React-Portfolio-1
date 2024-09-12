057b6088a09dd409c7d45fab010cfd611d2deb3c
Felix Cream-Dunlop <psyf2@nottingham.ac.uk>
2024-09-12 19:46:03 +0100

    feat: styling

--- react-portfolio/vite.config.js
+++ react-portfolio/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+  host: true,
+  port: 5173, // Make sure this matches the port you are using
+  },
+  
   plugins: [react()],
   css: {
     modules: {
